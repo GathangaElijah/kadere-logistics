e9f9f9de03402a5e321f436318ffca58c8ca5c24
GathangaElijah <elyg3672@gmail.com>
2026-01-08 15:24:03 +0300

    feat: added a button

--- kadere-frontend/app/page.tsx
+++ kadere-frontend/app/page.tsx
@@ -1,7 +1,9 @@
 export default function Home() {
   return (
     <div>
-      <h1 className="text-3xl font-bold underline">Welcome to Kadere Frontend</h1>
+      <div>
+        <button type="button">Book transport</button>
+      </div>
     </div>
   );
 }
