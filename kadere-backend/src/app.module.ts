31b0f301c3191cb8851d9bec66e108d15af62f9d
GathangaElijah <elyg3672@gmail.com>
2026-01-09 21:49:33 +0300

    chore: configuration

--- kadere-backend/src/app.module.ts
+++ kadere-backend/src/app.module.ts
@@ -1,10 +1,39 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import databaseConfig from './config/database.config';
+import { ConfigModule } from '@nestjs/config';
+import { DataSource } from 'typeorm';
+import { Driver } from './drivers/drivers.entity';
+
 
 @Module({
-  imports: [],
+  imports:[
+    ConfigModule.forRoot({
+    isGlobal: true,
+    load: [databaseConfig],
+  }),
+  TypeOrmModule.forRootAsync({
+    useFactory: async () => {
+      const config = databaseConfig();
+      return {
+        type: 'postgres',
+        host: config.database.host,
+        port: config.database.port,
+        username: config.database.username,
+        password: config.database.password,
+        database: config.database.dbName,
+        entities: [Driver],
+        synchronize: true
+      };
+    },
+  }),
+  ],
+
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+  constructor(private dataSource:DataSource) {}
+}
