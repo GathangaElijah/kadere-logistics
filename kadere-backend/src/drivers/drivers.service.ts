187b89033c637f61051e0d0c8ae1e4076f930a88
GathangaElijah <elyg3672@gmail.com>
2026-01-09 21:51:02 +0300

    chore: clean up

---
+++ kadere-backend/src/drivers/drivers.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { DriverDto } from 'src/dto/driver.dto';
+import { Repository } from 'typeorm';
+import { Driver } from './drivers.entity';
+
+@Injectable()
+export class DriversService {
+  constructor(
+    @InjectRepository(Driver)
+    private driversRepository: Repository<Driver>,
+  ) {}
+
+  async registerDriver(driverData: DriverDto) {
+    console.log(driverData);
+    return await this.driversRepository.save(driverData);
+  }
+}
--- kadere-backend/src/patners/patners.service.ts
+++
@@ -1,6 +0,0 @@
-import { Injectable } from '@nestjs/common';
-
-@Injectable()
-export class PatnersService {
-  registerDriver(driverData: DriverData) {}
-}
