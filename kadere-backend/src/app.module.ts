4d19ff5bd31e7d3447c631645559a756a55fc51f
GathangaElijah <elyg3672@gmail.com>
2026-01-10 16:17:25 +0300

    chore: import the drivers module to the app module

--- kadere-backend/src/app.module.ts
+++ kadere-backend/src/app.module.ts
@@ -6,34 +6,36 @@ import databaseConfig from './config/database.config';
 import { ConfigModule } from '@nestjs/config';
 import { DataSource } from 'typeorm';
 import { Driver } from './drivers/drivers.entity';
-
+import { DriversModule } from './drivers/drivers.module';
 
 @Module({
-  imports:[
+  imports: [
     ConfigModule.forRoot({
-    isGlobal: true,
-    load: [databaseConfig],
-  }),
-  TypeOrmModule.forRootAsync({
-    useFactory: async () => {
-      const config = databaseConfig();
-      return {
-        type: 'postgres',
-        host: config.database.host,
-        port: config.database.port,
-        username: config.database.username,
-        password: config.database.password,
-        database: config.database.dbName,
-        entities: [Driver],
-        synchronize: true
-      };
-    },
-  }),
+      isGlobal: true,
+      cache: true,
+      load: [databaseConfig],
+    }),
+    TypeOrmModule.forRootAsync({
+      useFactory: () => {
+        const config = databaseConfig();
+        return {
+          type: 'postgres',
+          host: config.database.host,
+          port: config.database.port,
+          username: config.database.username,
+          password: config.database.password,
+          database: config.database.dbName,
+          entities: [Driver],
+          synchronize: true,
+        };
+      },
+    }),
+    DriversModule,
   ],
 
   controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {
-  constructor(private dataSource:DataSource) {}
+  constructor(private dataSource: DataSource) {}
 }
