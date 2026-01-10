b3943f8f4422cdbaddc2b9a78e53c21e2d897af6
GathangaElijah <elyg3672@gmail.com>
2026-01-10 16:20:26 +0300

    chore: rename the files

--- kadere-frontend/app/lib/actions.ts
+++ kadere-frontend/app/lib/actions.ts
@@ -9,13 +9,13 @@ export async function registerDriver(form:FormData) {
     };
 
     try {
-        const response = await fetch('http://localhost:8000/api/patners/register-driver/', {
+        const response = await fetch('http://127.0.0.1:3000/drivers/register', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
             },
             body: JSON.stringify(rawData),
-        } );
+        });
         const result = await response.json();
         if (!response.ok) {
             throw new Error('Failed to register driver');     
